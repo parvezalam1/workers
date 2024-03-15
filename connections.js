9d49392218b0ed0e36be6a88a7bd2744e82d268e
saif <saif@gmail.com>
2024-03-15 15:08:29 +0530

    file changed

--- connections.js
+++ connections.js
@@ -2,7 +2,7 @@ let mysql=require('mysql')
 let conn=mysql.createConnection({
     host:'bczbu9o5wu2zl8eezy0d-mysql.services.clever-cloud.com',
     user:'uxyk6nenlrjok89z',
-    password:'uxyk6nenlrjok89z',
+    password:'FSZGg2c19ZYrTOoiaXWG',
     database:'bczbu9o5wu2zl8eezy0d'
 })
 module.exports=conn;
